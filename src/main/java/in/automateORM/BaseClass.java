6f748e374021af2c279840caf08ad7305ea5925f
tads <t.adityadhanrajsingh2001@gmail.com>
2024-01-23 22:44:58 +0530

    Added BaseClass and updated LoginPage

---
+++ src/main/java/in/automateORM/BaseClass.java
@@ -0,0 +1,23 @@
+package in.automateORM;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class BaseClass {
+	
+	// ############ Perform Login Function ###############
+	
+	public void performLogin(String username, String password, WebDriver driver) {
+
+		WebElement userField = driver.findElement(By.name("username"));
+		WebElement passwordField = driver.findElement(By.name("password"));
+
+		userField.sendKeys(username);
+		passwordField.sendKeys(password);
+
+		WebElement loginButton = driver.findElement(By.cssSelector("button[type='submit']"));
+		loginButton.click();
+
+	}
+}
