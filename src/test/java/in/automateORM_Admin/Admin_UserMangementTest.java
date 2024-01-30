ceea053dea58d6dc7038cb9662fb9dcb3fe78413
tads <t.adityadhanrajsingh2001@gmail.com>
2024-01-30 23:34:30 +0530

    Updated some design and testcases admin_UM

--- src/test/java/in/automateORM_Admin/Admin_UserMangementTest.java
+++ src/test/java/in/automateORM_Admin/Admin_UserMangementTest.java
@@ -10,6 +10,8 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import in.automateORM.loginPage;
 import in.automateORM.loginPageTest;
 import in.automateORM.sideMenuTest;
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -18,9 +20,9 @@ public class Admin_UserMangementTest {
 	
 	private WebDriver driver;
 	loginPageTest login = new loginPageTest();
-	sideMenuTest navigate = new sideMenuTest();
+	sideMenuTest navigate = new sideMenuTest();	
 	
-	
+	// *********** To be moved to base class ***********
 	
 	private void GoToAdminPage() {
 		
@@ -28,11 +30,6 @@ public class Admin_UserMangementTest {
 		search.sendKeys("Admin");
 		WebElement seach_Admin = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[1]/aside/nav/div[2]/ul/li/a"));
 		seach_Admin.click();
-		
-		if(driver.getCurrentUrl()!="https://opensource-demo.orangehrmlive.com/web/index.php/admin/viewSystemUsers") {
-			System.out.print("Not in desired URL");
-		}
-		
 	}
 	
 	@BeforeClass
@@ -49,13 +46,40 @@ public class Admin_UserMangementTest {
 		
 	}
 	
-
+	// ############# Positive cases for UserManagement #############
+	
 	@Test
-	public void ums_test() {
+	private void ums_test() {
+		// Checking Current page is admin -> UMS page.
+		
 		WebElement sysUsers = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div[1]/div[1]/div[1]/h5"));
 		Assert.assertTrue("Not found",sysUsers.isDisplayed());
 		
+		//Redirecting to users in UMS and checking URL 
+		WebElement UM_dropdown = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[1]/header/div[2]/nav/ul/li[1]/span"));
+		UM_dropdown.click();
+		WebElement Users = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[1]/header/div[2]/nav/ul/li[1]/ul/li"));
+		Users.click();
+		String currentURL = driver.getCurrentUrl();
+		String expectedURL = "https://opensource-demo.orangehrmlive.com/web/index.php/admin/viewSystemUsers";
+		Assert.assertTrue("Not on Users URL",currentURL!=expectedURL);
+		
 	}
+
+	//Making a common method for declaring web elements to move to base class
+
+	@Test
+	private void createUser_1() {
+		// Adding a new user 
+		
+		
+	}
+	
+	public void createUser_2() {
+		
+	}
+	
+	// ############# Negative cases for UserManagement #############
 	
 	@AfterClass
 	public void tearDown() {
